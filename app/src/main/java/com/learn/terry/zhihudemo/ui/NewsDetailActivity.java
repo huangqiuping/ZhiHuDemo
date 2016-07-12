8bc7aa1e3ce46fed2faf0fcc55a9790424f0fec4
hqp770 <hqp770@126.com>
2016-07-12 13:59:04 +0800

    使用ToolBar

--- app/src/main/java/com/learn/terry/zhihudemo/ui/NewsDetailActivity.java
+++ app/src/main/java/com/learn/terry/zhihudemo/ui/NewsDetailActivity.java
@@ -3,6 +3,7 @@ package com.learn.terry.zhihudemo.ui;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.webkit.WebView;
 
@@ -11,7 +12,7 @@ import com.learn.terry.zhihudemo.entity.News;
 import com.learn.terry.zhihudemo.task.LoadNewsDetailTask;
 import com.learn.terry.zhihudemo.utils.LogUtil;
 
-public class NewsDetailActivity extends AppCompatActivity implements NewsDetailWebView.OnScrollChangeListener{
+public class NewsDetailActivity extends AppCompatActivity implements NewsDetailWebView.OnScrollChangeListener {
     private NewsDetailWebView mWebView;
     private ActionBar mActionBar;
 
@@ -23,10 +24,16 @@ public class NewsDetailActivity extends AppCompatActivity implements NewsDetailW
         News news = (News) getIntent().getSerializableExtra("news");
         LogUtil.log(news.toString());
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
+
         mWebView = (NewsDetailWebView) findViewById(R.id.web_view);
         assert mWebView != null;
         mWebView.setOnScrollChangeListener(this);
         mActionBar = getSupportActionBar();
+        if (mActionBar != null) {
+            mActionBar.setDisplayHomeAsUpEnabled(true);
+        }
 
         configWebView(mWebView);
 
@@ -41,10 +48,22 @@ public class NewsDetailActivity extends AppCompatActivity implements NewsDetailW
 
     @Override
     public void onScrollChange(int l, int t, int oldl, int oldt) {
+        LogUtil.log("onScrollChange: l = " + l + ", t = " + t + ", oldl = " + oldl + ", oldt = " + oldt);
         if (oldt < t) {
-            mActionBar.hide();
+            updateActionBar(false);
         } else {
+            updateActionBar(true);
+        }
+    }
+
+    private void updateActionBar(boolean isShow) {
+        if (mActionBar == null) {
+            return;
+        }
+        if (isShow) {
             mActionBar.show();
+        } else {
+//            mActionBar.hide();
         }
     }
 }
